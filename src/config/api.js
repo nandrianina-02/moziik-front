140417eb226882820bf9493c7e64318838aa3c96
Nandrianina <dodisoa.nandrianina@gmail.com>
2026-04-17 12:49:33 +0200

    remove api key

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 export const API = 'https://moozik-gft1.onrender.com';
+const API_KEY = import.meta.env.VITE_ANTHROPIC_API_KEY;
 export const ANTHROPIC_CONFIG = {
-  API_KEY: import.meta.env.VITE_ANTHROPIC_API_KEY || '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',
+  API_KEY: API_KEY,
   VERSION: '2023-06-01',
   BASE_URL: 'https://api.anthropic.com/v1/messages',
   DEFAULT_MODEL: 'claude-3-5-sonnet-20240620'
